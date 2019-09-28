20c1de2894ea0c87459ad63d6708a99943db2170
nelson sepulveda <elchetodelciber_95@hotmail.com>
2019-09-28 12:29:05 -0300

    trabajando el signin

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,8 +5,7 @@ router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 })
 
-router.get('/users/signup', (req, res) => {
-    res.render('users/signup');
+router.post('/users/signin',(req,res)=>{
+    req  //completar
 })
-
 module.exports = router;
