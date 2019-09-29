f9ed9a9857c7895f3c1c2587477644e24a0e14d8
Nelson Sepulveda <nelsonlautarosepulveda@gmail.com>
2019-09-29 18:56:42 -0300

    user y pass verificados

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,23 @@ router.get('/users/signin', (req, res) => {
     res.render('users/signin');
 })
 
-router.post('/users/signin',(req,res)=>{
-    req  //completar
+router.post('/users/signin', (req,res) => {
+    console.log(req.body);
+    const {email,password}=req.body; //se toman los datos enviados desde el front
+
+    const errors=[];       //si hay errores se guardan aqui
+    if(email.length<5){
+        errors.push({text:'Ingrese un email valido'});
+    }
+    if(password.length<6){
+        errors.push({text: 'La contraseña de demasiado corta'});
+    }
+    if(errors.length>0){
+        res.render('users/signin', {errors , email, password});  //si hay errores  se carga la misma pagina pero se le envian los errores 
+    }
+    else{
+        res.send('ok');
+    }
+    
 })
 module.exports = router;
