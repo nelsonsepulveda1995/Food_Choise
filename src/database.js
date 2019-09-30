33f89899ed331372ff5b4300c55aa74e6b2abefd
dElias <eliasdominguez095@gmail.com>
2019-09-30 19:36:03 -0300

    Update database
    Actualizacion de la conexion a la base de datos. Ahora se encuentra en la nube.

--- src/database.js
+++ src/database.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/recipes-db-app', {
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://elias:<elias>@foodchoise-sybkf.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    console.log("conectado wey")
+  client.close();
+});
+
+/*mongoose.connect('mongodb://localhost/recipes-db-app', {    
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
 })
     .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err));*/
