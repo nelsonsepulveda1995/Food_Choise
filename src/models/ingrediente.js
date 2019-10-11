43848bc02bb9dc7594e571e3e2a5857d7f644a05
dElias <eliasdominguez095@gmail.com>
2019-10-10 23:57:37 -0300

    Recetas
    Cambios en receta y nuevos modelos

---
+++ src/models/ingrediente.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+    _id:{ type: String, required: true },
+    descripcion: { type: String, required: true },    
+});
+
+module.exports = mongoose.model('ingrediente',recipeSchema);
