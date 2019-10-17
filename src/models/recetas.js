dfc3002711e47dcdc55e3586dcf10ea28985e3af
Nelson Sepulveda <nelsonlautarosepulveda@gmail.com>
2019-10-17 19:54:05 -0300

    Update recetas.js

--- src/models/recetas.js
+++ src/models/recetas.js
@@ -8,7 +8,8 @@ const recipeSchema = new Schema({  //falta agregar ingredientes, calificacion, p
     date: { type: Date, default: Date.now },
     categoria: {type: String, require: true},
     imagenURL:{type: String, required: true},   //url de la imagen dada por cloudinary
-    imagenCloud:{type:String, required: true}   //id unico de la foto guardada en cloudinari para poder borrarla
+    imagenCloud:{type:String, required: true},   //id unico de la foto guardada en cloudinari para poder borrarla
+    ingredientes:{type:Array,required:true}
 });
 
 module.exports = mongoose.model('receta',recipeSchema);
